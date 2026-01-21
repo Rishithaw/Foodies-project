f2bd050d33a4665877ce7651eae0ac79271c19db
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-21 10:32:17 -0600

    Implemented NavLink component for navigation

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,8 +3,10 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
 import Image from "next/image";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground />
@@ -16,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
